df9eed934cb7b1bae5f682ca4ddfff79a655681e
cbohlmanaz <cbohlman@email.arizona.edu>
2017-02-10 18:11:47 -0700

    Add files via upload

--- project_files/linearityCodeNov20142Data.py
+++ project_files/linearityCodeNov20142Data.py
@@ -14,57 +14,43 @@ Subtract image 1 - image 4
 Write that to a fits file
 
 """
-
-imageAmount = 21
-raw_ints = []
-raw_counts = []
-ints = []
-counts = []
-true_counts_1 = []
-true_counts_2 = []
-true_counts_3 = []
-true_counts_4 = []
-error_0 = []
-error_1 = []
-error_2 = []
-error_3 = []
-error_4 = []
-true_counts = []
-corrected = []
-
-lower = 60
-lower_adj = 5
-upper = 109
-upper_adj = 8
-xarr = numpy.linspace(5000,60000,21)
 i = 0
-x = 0
-y = 0
-count = 0
-ds9x2 = 400
-ds9y2 = 200
-coefs_inv = [3.67e-16, -7.58e-11, 2.69e-06, 9.66e-01, 1.39e+02]
-s = (200,400)
-array = numpy.zeros(s)
-
 while i < 4:
+	imageAmount = 21
+	raw_ints = []
+	raw_counts = []
+	ints = []
+	counts = []
+	
+	lower = 60
+	lower_adj = 5
+	upper = 109
+	upper_adj = 8
+	x = 0
+	y = 0
+	count = 0
+	ds9x2 = 400
+	ds9y2 = 200
+	coefs_inv = [3.67e-16, -7.58e-11, 2.69e-06, 9.66e-01, 1.39e+02]
+	myList = []
+
 	fname = "tau_0000"+str(i+1)+".fit"
 	hdu1 = fits.open(fname)
 	scidata = hdu1[0].data
-	
-	w, h = ds9x2, ds9y2
 	array = numpy.zeros((ds9y2,ds9x2))
-	
+
 	while x < ds9x2:
 		while y < ds9y2:
-			num = (float)scidata[y:y,x:x]
-			print(str(num)+"\n")
+			str_num = str(scidata[0,y,x])
+			num = float(str_num)
 			new_num = num*num*num*num*coefs_inv[0] + num*num*num*coefs_inv[1] + num*num*coefs_inv[2] + num*coefs_inv[3] + coefs_inv[4]
 			array[y][x] = new_num
-			y = y+ 1
+			myList.append(new_num)
+			y = y + 1
+		y = 0
 		x = x + 1
-	
-	hdu = fits.PrimaryHDU(array)
+
+	hdu = fits.PrimaryHDU(myList)
 	hdu.writeto('FIXED_IMG'+str(i+1)+'.fits', clobber = True)
 	i = i + 1
 	
